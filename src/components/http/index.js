fc134f84f5e38444088b14fc959f8a8c7af906b4
Maxime Sahroui <maxime.sahroui@gmail.com>
2018-10-14 18:10:39 +0200

    Add dispatch and match fns

--- src/components/http/index.js
+++ src/components/http/index.js
@@ -1,5 +1,8 @@
 const micro = require('micro');
-const {send} = require('micro');
+const dispatch = require('micro-route/dispatch');
+const match = require('micro-route/match');
+
+const {send} = micro;
 
 const status = n => (req, res) => send(res, n);
 
@@ -11,11 +14,13 @@ const compose = (...fns) =>
 		fn => async (...args) => await fn(...args) // eslint-disable-line no-return-await
 	);
 
-const listen = server => port => fn => server.listen(port, fn);
+const listen = server => (port, fn) => server.listen(port, fn());
 
 module.exports = {
-	status,
 	server,
+	status,
 	compose,
-	listen
+	listen,
+	dispatch,
+	match
 };
